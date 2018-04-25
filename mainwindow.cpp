8fce4340dff3c6eaf978cc2e4b4279e621c47837
Aleksandr Bobõrev <aleksandr.bobyrev@tthk.ee>
2018-04-25 18:01:52 +0300

    last commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,9 +3,10 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow )
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow( )
